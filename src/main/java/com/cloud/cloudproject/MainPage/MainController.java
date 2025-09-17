d145edc53cf0634282a4e03d3ebff1d26013802c
won24 <iamwon942@gmail.com>
2025-09-17 12:37:51 +0900

    mainConroller 수정

--- src/main/java/com/cloud/cloudproject/MainPage/MainController.java
+++ src/main/java/com/cloud/cloudproject/MainPage/MainController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+//    @RequestMapping("/")
+//    public String index(Model model) {
+//        model.addAttribute("title", "비드온 - 경매사이트");
+//        model.addAttribute("contentPage", "../main/index.jsp");
+//        return "common/layout";
+//    }
+
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "비드온 - 경매사이트");
-        model.addAttribute("contentPage", "../main/index.jsp");
-        return "common/layout";
+        return "main/index";
     }
 
     @RequestMapping("/terms")
