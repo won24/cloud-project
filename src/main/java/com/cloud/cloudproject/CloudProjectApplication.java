6e24e3baed9362b0fe1cfa050962ab630b36a05d
won24 <iamwon942@gmail.com>
2025-09-17 14:45:09 +0900

    CloudProjectApplication.java 수정

--- src/main/java/com/cloud/cloudproject/CloudProjectApplication.java
+++ src/main/java/com/cloud/cloudproject/CloudProjectApplication.java
@@ -2,8 +2,13 @@ package com.cloud.cloudproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.cloud.cloudproject",
+        "com.cloud.cloudproject.MainPage"  // 명시적으로 추가
+})
 public class CloudProjectApplication {
 
     public static void main(String[] args) {
